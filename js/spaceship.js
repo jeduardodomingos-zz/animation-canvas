0309ae2d36e81ba322cdcf08c22c2eb68592bf3a
Gabriel Padilha <gabriel.padilha@cvh.com.br>
2019-04-17 13:06:40 -0300

    Funcionalidade de Game Over adicionada

--- js/spaceship.js
+++ js/spaceship.js
@@ -6,6 +6,7 @@ export class Spaceship extends BaseSpaceship {
 
     constructor() {
         super();
+        this.gameOver = false;
         this.enemies = [];
     }
 
@@ -21,36 +22,26 @@ export class Spaceship extends BaseSpaceship {
         enemy.canvasAreaName = "canvas-area";
         enemy.layoutPath = '../images/enemy-spaceship.png';
 
-        if (this.enemies.length > 0) {
+        if (!this.gameOver) {
+            if (this.enemies.length > 0) {
 
-            lastEnemy = this.enemies[this.enemies.length - 1];
+                lastEnemy = this.enemies[this.enemies.length - 1];
 
-            if (((lastEnemy.lastXPosition == null ? lastEnemy.initialXPosition : lastEnemy.lastXPosition)) < (canvas.width - lastEnemy.colisionX)) {
-                enemy.initialXPosition = (lastEnemy.lastXPosition == null ? lastEnemy.initialXPosition : lastEnemy.lastXPosition) + enemy.marginX;
-                enemy.initialYPosition = (lastEnemy.lastYPosition == null ? lastEnemy.initialYPosition : lastEnemy.lastYPosition);
+                if (((lastEnemy.lastXPosition == null ? lastEnemy.initialXPosition : lastEnemy.lastXPosition)) < (canvas.width - lastEnemy.colisionX)) {
+                    enemy.initialXPosition = (lastEnemy.lastXPosition == null ? lastEnemy.initialXPosition : lastEnemy.lastXPosition) + enemy.marginX;
+                    enemy.initialYPosition = (lastEnemy.lastYPosition == null ? lastEnemy.initialYPosition : lastEnemy.lastYPosition);
+                } else {
+                    enemy.initialXPosition = 0 + enemy.marginX;
+                    enemy.initialYPosition = (lastEnemy.lastYPosition == null ? lastEnemy.initialYPosition : lastEnemy.lastYPosition) + enemy.marginY;
+                }
             } else {
                 enemy.initialXPosition = 0 + enemy.marginX;
-                enemy.initialYPosition = (lastEnemy.lastYPosition == null ? lastEnemy.initialYPosition : lastEnemy.lastYPosition) + enemy.marginY;
+                enemy.initialYPosition = 0 + enemy.marginY;
             }
-        } else {
-            enemy.initialXPosition = 0 + enemy.marginX;
-            enemy.initialYPosition = 0 + enemy.marginY;
-        }
 
-        if ((enemy.lastYPosition == null ? enemy.initialYPosition : enemy.lastYPosition) + enemy.marginY < canvas.height - enemy.colisionY * 3) {
-
-            let fireCount = this.shots.length;
-
-            let newShot = new Shot();
-
-            newShot.shotNumber = fireCount + 1;
-            newShot.marginX = enemy.marginX;
-            newShot.marginY = enemy.marginY + 20;
-            newShot.shotXPositon = (enemy.lastXPosition == null ? enemy.initialXPosition : enemy.lastXPosition) + newShot.marginX;
-            newShot.shotYPosition = (enemy.lastYPosition == null ? enemy.initialYPosition : enemy.lastYPosition) + newShot.marginY;
-
-            enemy.shots.push(newShot);
-            this.enemies.push(enemy);
+            if ((enemy.lastYPosition == null ? enemy.initialYPosition : enemy.lastYPosition) + enemy.marginY < canvas.height - enemy.colisionY * 2.5) {
+                this.enemies.push(enemy);
+            }
         }
     }
 
@@ -82,13 +73,15 @@ export class Spaceship extends BaseSpaceship {
 
         let newShot = new Shot();
 
-        newShot.shotNumber = fireCount + 1;
-        newShot.marginX = 56;
-        newShot.marginY = 15;
-        newShot.shotXPositon = this.lastXPosition + newShot.marginX;
-        newShot.shotYPosition = this.lastYPosition + newShot.marginY;
+        if (!this.gameOver) {
+            newShot.shotNumber = fireCount + 1;
+            newShot.marginX = 56;
+            newShot.marginY = 15;
+            newShot.shotXPositon = this.lastXPosition + newShot.marginX;
+            newShot.shotYPosition = this.lastYPosition + newShot.marginY;
 
-        this.shots.push(newShot);
+            this.shots.push(newShot);
+        }
     }
 
     spaceshipMove(left, right, top, bottom, increment) {
@@ -99,19 +92,23 @@ export class Spaceship extends BaseSpaceship {
         this.lastXPosition = this.lastXPosition == null ? this.initialXPosition : this.lastXPosition;
         this.lastYPosition = this.lastYPosition == null ? this.initialYPosition : this.lastYPosition;
 
+        this.makeEnemy();
+
         this.clearCanvas();
 
-        if (left && this.lastXPosition >= 0) {
-            this.lastXPosition -= increment;
-        } else if (right && this.lastXPosition < canvas.width - this.colisionX) {
-            this.lastXPosition += increment;
-        } else if (bottom && this.lastYPosition < canvas.height - this.colisionY) {
-            this.lastYPosition += increment;
-        } else if (top && this.lastYPosition > canvas.height / 2) {
-            this.lastYPosition -= increment;
-        }
+        if (!this.gameOver) {
+            if (left && this.lastXPosition >= 0) {
+                this.lastXPosition -= increment;
+            } else if (right && this.lastXPosition < canvas.width - this.colisionX) {
+                this.lastXPosition += increment;
+            } else if (bottom && this.lastYPosition < canvas.height - this.colisionY) {
+                this.lastYPosition += increment;
+            } else if (top && this.lastYPosition > canvas.height / 2) {
+                this.lastYPosition -= increment;
+            }
 
-        this.drawSpaceship(this.layoutPath);
+            this.drawSpaceship(this.layoutPath);
+        }
     }
 
     drawFire() {
@@ -153,6 +150,21 @@ export class Spaceship extends BaseSpaceship {
         this.enemies.forEach((enemy) => {
             let enm = new Image();
             enm.src = '../images/enemy-spaceship.png';
+
+            enemy.lastXPosition = enemy.lastXPosition == null ? enemy.initialXPosition : enemy.lastXPosition;
+            enemy.lastYPosition = enemy.lastYPosition == null ? enemy.initialYPosition : enemy.lastYPosition + 0.1;
+
+            if (enemy.lastYPosition >= canvas.height - this.colisionY) {
+                let gameOver = new Image();
+
+                gameOver.src = '../images/gameover.png';
+
+                this.clearCanvas();
+                this.gameOver = true;
+                context.drawImage(gameOver, (canvas.width / 2) - 200, (canvas.height / 2) - 200);
+
+                return;
+            }
 
             context.drawImage(enm, enemy.lastXPosition == null ? enemy.initialXPosition : enemy.lastXPosition, enemy.lastYPosition == null ? enemy.initialYPosition : enemy.lastYPosition);
         });
